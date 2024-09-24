d0444b705f945dc3d55ce84c042a947a4ffb7cb4
rohanveit1 <rohan.veit@gmail.com>
2024-09-24 16:10:53 +0100

    inital commit

---
+++ CW1_Try2/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CW1_Try2
+{
+    public partial class Form1 : Form
+    {
+
+        HttpClient client;
+
+        public Form1()
+        {
+            InitializeComponent();
+            this.client = new HttpClient();
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private async void onTextboxEnter(object sender, System.Windows.Forms.KeyPressEventArgs e)
+        {
+            if (e.KeyChar == (char)Keys.Return)
+
+            {
+                TextBox box = (TextBox)sender;
+                using HttpResponseMessage response = await client.GetAsync(box.Text);
+                label2.Text = "" + ((int) response.StatusCode) + " - " + response.StatusCode.ToString(); //TODO: Use string formatting to make this nicer
+                response.EnsureSuccessStatusCode();
+
+                string responseBody = await response.Content.ReadAsStringAsync();
+                label1.Text = responseBody;
+
+            }
+        }
+    }
+}
---
+++ CW1_Try2/notes/TextFile1.md
@@ -0,0 +1,5 @@
+﻿# 24/09/2024
+- Made winforms project
+- Researched HttpClient class
+- Implemented TextBox + HttpClient Request functionality
+- Added label denoted status code (+ meaning of code)
